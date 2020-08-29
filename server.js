52c0efea1ef12b3d7af2d1492398b79db573a7f7
kunalkumar007 <kunalkumar06022003@gmail.com>
2020-08-29 20:21:31 +0530

    done

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ app.use(express.json());
 app.use(cors());
 app.use('/', require('./routes/exercise-list.route'));
 
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+	app.get('*',(req,res)=>{
+		res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+	})
+
 mongoose.connect(
 	`mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.wintt.mongodb.net/<dbname>?retryWrites=true&w=majority`,
 	{ useNewUrlParser: true, useUnifiedTopology: true },
